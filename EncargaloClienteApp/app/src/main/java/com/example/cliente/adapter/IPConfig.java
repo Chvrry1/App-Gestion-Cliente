96af484d795125c074d11bbb98c4a4bf04c7befd
Chvrry1 <chvrry69@gmail.com>
2024-10-23 15:18:48 -0500

    funciona2

--- EncargaloClienteApp/app/src/main/java/com/example/cliente/adapter/IPConfig.java
+++ EncargaloClienteApp/app/src/main/java/com/example/cliente/adapter/IPConfig.java
@@ -3,6 +3,6 @@ package com.example.cliente.adapter;
 public class IPConfig {
 
   //  public static final String ipServidor = "http://192.168.1.125:2020/APIS/";
-    public static final String ipServidor = "http://192.168.0.112/app_finanzas/";
+    public static final String ipServidor = "http://192.168.x.x/app_finanzas/";
 
 }
